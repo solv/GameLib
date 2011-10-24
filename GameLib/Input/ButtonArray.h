520d10a07633dff61553b4e36ec377234526fe7d
solv <solv301@gmail.com>
2011-10-24 21:46:18 +0900

    ジョイパッドからの入力のためのキー番号

---
+++ GameLib/Input/ButtonArray.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// ジョイパッドのキー番号
+namespace ButtonArray {
+	enum Button {
+		BUTTON1 = 0,
+		BUTTON2,
+		BUTTON3,
+		BUTTON4,
+		BUTTON5,
+		BUTTON6,
+		BUTTON7,
+		BUTTON8,
+		BUTTON9,
+		BUTTON10,
+		BUTTON11,
+		BUTTON12,
+		BUTTON13,
+		BUTTON14,
+		BUTTON15,
+		BUTTON16
+	};
+}
